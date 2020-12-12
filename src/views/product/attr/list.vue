0aed8511cd0bb9c5a5f009fc9c4ddfd568c7d9bb
keyLJet <keyLJet@hotmail.com>
2020-12-12 16:17:09 +0800

    feat(attrList&category):获取数据并展示

--- src/views/product/attr/list.vue
+++ src/views/product/attr/list.vue
@@ -1,28 +1,8 @@
 <template>
   <div>
     <!-- 属性分类选择区域 -->
-    <el-card>
-      <el-form inline :model="category">
-        <el-form-item label="一级分类">
-          <el-select v-model="category.category1Id" placeholder="请选择">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="二级分类">
-          <el-select v-model="category.category2Id" placeholder="请选择">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="三级分类">
-          <el-select v-model="category.category3Id" placeholder="请选择">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
-          </el-select>
-        </el-form-item>
-      </el-form>
-    </el-card>
+    <Category @change='getAttrList' />
+
     <!-- 平台属性列表及操作区域 -->
     <el-card style="margin-top: 20px">
       <!-- 添加属性 -->
@@ -31,35 +11,62 @@
       <el-table :data="attrList" border style="width: 100%; margin: 20px 0">
         <el-table-column type="index" label="序号" width="80" align="center">
         </el-table-column>
-        <el-table-column prop="tmName" label="属性名称" width="150">
+        <el-table-column prop="attrName" label="属性名称" width="150">
         </el-table-column>
 
-        <el-table-column label="属性值列表"> </el-table-column>
+         <el-table-column label="属性值列表">
+          <template v-slot="{ row }">
+            <el-tag
+              style="margin-right: 5px"
+              v-for="attrVal in row.attrValueList"
+              :key="attrVal.id"
+              >{{ attrVal.valueName }}</el-tag
+            >
+          </template>
+        </el-table-column>
 
         <el-table-column label="操作" width="150">
           <template>
-            <el-button type="warning" icon="el-icon-edit">修改</el-button>
-            <el-button type="danger" icon="el-icon-delete">删除</el-button>
+            <el-button
+              type="warning"
+              icon="el-icon-edit"
+              size="mini"
+            ></el-button>
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              size="mini"
+            ></el-button>
           </template>
         </el-table-column>
+        
       </el-table>
     </el-card>
-    
   </div>
 </template>
 
 <script>
+import Category from "./category";
+
 export default {
-  name: 'AttrList',
-  data(){
+  name: "AttrList",
+  data() {
     return {
-      category:{
-        category1Id:'',
-        category2Id:'',
-        category3Id:'',
+      category: {
+        category1Id: "",
+        category2Id: "",
+        category3Id: "",
       },
-      attrList:[],
-    }
+      attrList: [],
+    };
   },
-}
+  methods:{
+    getAttrList(attrList) {
+      this.attrList = attrList;
+    },
+  },
+  components: {
+    Category,
+  },
+};
 </script>
