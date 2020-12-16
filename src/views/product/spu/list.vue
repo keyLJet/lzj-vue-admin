8856871af04f6556b5267db5d8a3e77019b7cca5
keyLJet <keyLJet@hotmail.com>
2020-12-16 23:48:45 +0800

    feat(SkuList):静态组件，请求数据并遍历展示

--- src/views/product/spu/list.vue
+++ src/views/product/spu/list.vue
@@ -1,44 +1,55 @@
 <template>
-<div>
-  <Category :disabled='!isShowList' />
-  <SpuShowList v-if="isShowList" @showUpdateList='showUpdateList' />
-  <SpuUpdateList v-else :item='item' @showList='showList' />
+  <div>
+    <SkuList v-if="isShowSkuList" :spuItem='spuItem' />
 
-</div>
+    <div v-else>
+      <Category :disabled="!isShowList" />
+      <SpuShowList v-if="isShowList" @showUpdateList="showUpdateList" @showSkuList='showSkuList' />
+      <SpuUpdateList v-else :item="item" @showList="showList" />
+    </div>
+  </div>
 </template>
 
 <script>
-import Category from '@/components/Category'
-import SpuShowList from './spuShowList'
-import SpuUpdateList from './spuUpdateList'
+import Category from "@/components/Category";
+import SpuShowList from "./spuShowList";
+import SpuUpdateList from "./spuUpdateList";
+import SkuList from "./skuList";
 
 export default {
-  name: 'SpuList',
+  name: "SpuList",
   data() {
     return {
-      isShowList:true,
-      item:{},
-    }
+      isShowList: true,
+      item: {},
+      isShowSkuList: false,
+      spuItem:{},
+    };
   },
-  methods:{
+  methods: {
     // SpuShowList中点击编辑按钮时触发，传递参数row
-    showUpdateList(row){
-      this.isShowList = false
-      this.item = {...row}
+    showUpdateList(row) {
+      this.isShowList = false;
+      this.item = { ...row };
     },
     //SpuUpdateList组件中点击保存更新spu成功或者点击取消时触发，传递参数category3Id
-    showList(category3Id){
-      this.isShowList = true
+    showList(category3Id) {
+      this.isShowList = true;
       //等SpuShowList组件页面挂载成功后触发change事件重新获取spu分页列表
-      this.$nextTick(() =>{
-        this.$bus.$emit('change',{category3Id})
-      })
+      this.$nextTick(() => {
+        this.$bus.$emit("change", { category3Id });
+      });
+    },
+    showSkuList(row) {
+      this.isShowSkuList = true
+      this.spuItem = {...row}
     },
   },
   components: {
     Category,
     SpuShowList,
     SpuUpdateList,
-   },
-}
+    SkuList,
+  },
+};
 </script>
